cc5e018e43e0bdb28e74c8a7570b016e902417be
Dayvson Vinícius <dayvson123@bitbucket.org>
2021-10-08 15:36:51 -0300

    Removendo Consoles.log, Ajustando Tabulação e Adicionando event.preventDefault no Next e Previous

--- src/dvCalendar.js
+++ src/dvCalendar.js
@@ -48,76 +48,76 @@ try{
   showCalendar(currentMonth, currentYear);
 
   const newCurrentDate = (newDate = '') => {
-    console.log(newDate)
     let [d,m,y] = newDate.split('/')
     today = new Date(`${m}/${d}/${y}`)
     currentMonth = today.getMonth()
     currentYear = today.getFullYear()
   }
 
-  const next = () => {
+  const next = (e) => {
+    e.preventDefault()
     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
     currentMonth = (currentMonth + 1) % 12;
     showCalendar(currentMonth, currentYear);
   }
 
-  const previous = () => {
-      currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-      currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-      showCalendar(currentMonth, currentYear);
+  const previous = (e) => {
+    e.preventDefault()
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
   }
 
   const jump = () => {
-      currentYear = parseInt(selectYear.value);
-      currentMonth = parseInt(selectMonth.value);
-      showCalendar(currentMonth, currentYear);
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
   }
 
   function showCalendar(month, year) {
-    console.log(month, year)
 
-      let firstDay = (new Date(year, month)).getDay();
+    let firstDay = (new Date(year, month)).getDay();
 
-      tbl = document.querySelector(`#${config.dv} .dv-calendar-body`); // body of the calendar
+    tbl = document.querySelector(`#${config.dv} .dv-calendar-body`); // body of the calendar
 
-      // clearing all previous cells
-      tbl.innerHTML = "";
+    // clearing all previous cells
+    tbl.innerHTML = "";
 
-      selectYear.value = year;
-      selectMonth.value = month;
+    selectYear.value = year;
+    selectMonth.value = month;
 
-      // creating all cells
-      let date = 1;
-      for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+      // creates a table row
+      let row = document.createElement("tr");
 
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-          if (i === 0 && j < firstDay) {
-            cell = document.createElement("td");
-            cellText = document.createTextNode("");
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-          }
-          else if (date > daysInMonth(month, year)) {
-            break;
-          }
-
-          else {
-            cell = document.createElement("td");
-            cellText = document.createTextNode(date);
-            if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth())
-              cell.classList.add("currentDay");// color today's date
-
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-            date++;
-          }
-          cell.innerHTML.length > 0 ? cell.addEventListener('click', (e) => dateInInput(e.target.innerHTML,currentMonth,currentYear )) : false
+      //creating individual cells, filing them up with data.
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < firstDay) {
+          cell = document.createElement("td");
+          cellText = document.createTextNode("");
+          cell.appendChild(cellText);
+          row.appendChild(cell);
         }
-        tbl.appendChild(row); // appending each row into calendar body.
+        else if (date > daysInMonth(month, year)) {
+          break;
+        }
+
+        else {
+          cell = document.createElement("td");
+          cellText = document.createTextNode(date);
+          if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth())
+            cell.classList.add("currentDay");// color today's date
+
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+          date++;
+        }
+        cell.innerHTML.length > 0 ? cell.addEventListener('click', (e) => dateInInput(e.target.innerHTML,currentMonth,currentYear )) : false
       }
+      tbl.appendChild(row); // appending each row into calendar body.
+    }
 
   }
 
@@ -125,7 +125,6 @@ try{
     const selectedDate = new Date(`${m+1}/${d}/${y}`)
     inputDate.value = selectedDate.toLocaleDateString()
     newCurrentDate(selectedDate.toLocaleDateString())
-    console.log(selectedDate.toLocaleDateString())
     showCalendar(currentMonth, currentYear)
   }
 
